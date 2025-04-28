7edf2403f574b4d7a0b8e839ee68d10957357346
yourflowrs <violietaflow@gmail.com>
2025-04-28 13:20:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  function tampil (){
+    document. getElementById ("Alert").style.display ="block";
+     document. getElementById ("overlay").style.display ="block";
+  }
+  function tutup (){
+    document. getElementById ("Alert").style.display ="none";
+     document. getElementById ("overlay").style.display ="none";
+  }
+  }
